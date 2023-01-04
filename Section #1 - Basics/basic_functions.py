163eb50ebbd5c00d5dfe1436b67f93506cedc0ff
Jerry <tcjwang@outlook.com>
2023-01-04 10:30:18 -0800

    lesson 2

--- Section #1 - Basics/basic_functions.py
+++ Section #1 - Basics/basic_functions.py
@@ -7,29 +7,37 @@ img = cv.imread('../Resources/Photos/park.jpg')
 cv.imshow('Park', img)
 
 # Converting to grayscale
+# change image to gray
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow('Gray', gray)
 
 # Blur 
+# blur the image
 blur = cv.GaussianBlur(img, (7,7), cv.BORDER_DEFAULT)
 cv.imshow('Blur', blur)
 
 # Edge Cascade
+# find the edges of the image, if blured, then less edges will be found, making the image clearer
 canny = cv.Canny(blur, 125, 175)
 cv.imshow('Canny Edges', canny)
 
 # Dilating the image
+# 膨胀线条 set the lines of the canny image wider
 dilated = cv.dilate(canny, (7,7), iterations=3)
 cv.imshow('Dilated', dilated)
 
 # Eroding
+# 和膨胀相反，收缩线条，eroded is the same as canny
 eroded = cv.erode(dilated, (7,7), iterations=3)
 cv.imshow('Eroded', eroded)
 
 # Resize
+# change size of imag to (500, 500) without losing any part of image
 resized = cv.resize(img, (500,500), interpolation=cv.INTER_CUBIC)
 cv.imshow('Resized', resized)
 
 # Cropping
+# fetch part of the image
+# [top_left, bottom_right]
 cropped = img[50:200, 200:400]
 cv.imshow('Cropped', cropped)
--- Section #1 - Basics/draw.py
+++ Section #1 - Basics/draw.py
@@ -3,27 +3,28 @@
 import cv2 as cv
 import numpy as np
 
-blank = np.zeros((500,500,3), dtype='uint8')
-cv.imshow('Blank', blank)
+mat = np.zeros((500,500,3), dtype='uint8')
+cv.imshow('Blank', mat)
 
 # 1. Paint the image a certain colour
-blank[200:300, 300:400] = 0,0,255
-cv.imshow('Green', blank)
+mat[200:300, 300:400] = 0,255,0
+cv.imshow('Green', mat)
 
 # 2. Draw a Rectangle
-cv.rectangle(blank, (0,0), (blank.shape[1]//2, blank.shape[0]//2), (0,255,0), thickness=-1)
-cv.imshow('Rectangle', blank)
+# thickness=cv.FILLED is the same as thickness=-1
+cv.rectangle(mat, (0,0), (mat.shape[1]//2, mat.shape[0]//2), (255,0,0), thickness=cv.FILLED)
+cv.imshow('Rectangle', mat)
 
 # 3. Draw A circle
-cv.circle(blank, (blank.shape[1]//2, blank.shape[0]//2), 40, (0,0,255), thickness=-1)
-cv.imshow('Circle', blank)
+cv.circle(mat, (mat.shape[1]//2, mat.shape[0]//2), 40, (0,0,255), thickness=-1)
+cv.imshow('Circle', mat)
 
 # 4. Draw a line
-cv.line(blank, (100,250), (300,400), (255,255,255), thickness=3)
-cv.imshow('Line', blank)
+cv.line(mat, (100,250), (300,400), (255,255,255), thickness=3)
+cv.imshow('Line', mat)
 
 # 5. Write text
-cv.putText(blank, 'Hello, my name is Jason!!!', (0,225), cv.FONT_HERSHEY_TRIPLEX, 1.0, (0,255,0), 2)
-cv.imshow('Text', blank)
+cv.putText(mat, 'Hello, my name is Jason!!!', (0,225), cv.FONT_HERSHEY_TRIPLEX, 1.0, (128,128,128), 2)
+cv.imshow('Text', mat)
 
 cv.waitKey(0)
