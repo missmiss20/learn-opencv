80a3b04190dc69707878cc0f75785ab4defa934e
Jerry <tcjwang@outlook.com>
2023-01-05 11:26:10 -0800

    Update contours.py

--- Section #1 - Basics/contours.py
+++ Section #1 - Basics/contours.py
@@ -12,14 +12,16 @@ cv.imshow('Blank', blank)
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow('Gray', gray)
 
+# a way to find contours, first blur the image to reduce the amount of contours, then find countours
 blur = cv.GaussianBlur(gray, (5,5), cv.BORDER_DEFAULT)
 cv.imshow('Blur', blur)
 
 canny = cv.Canny(blur, 125, 175)
 cv.imshow('Canny Edges', canny)
 
-# ret, thresh = cv.threshold(gray, 125, 255, cv.THRESH_BINARY)
-# cv.imshow('Thresh', thresh)
+# another way to find contours by reducing light locor to 0
+ret, thresh = cv.threshold(gray, 125, 255, cv.THRESH_BINARY)
+cv.imshow('Thresh', thresh)
 
 contours, hierarchies = cv.findContours(canny, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
 print(f'{len(contours)} contour(s) found!')
